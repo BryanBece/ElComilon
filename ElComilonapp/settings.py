b8cd3d57097c44f979b524e0b3b3583d174a37e5
BryanBecerra <br.becerra.p@gmail.com>
2023-06-08 21:57:56 -0400

    superuser

--- ElComilonapp/settings.py
+++ ElComilonapp/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
 ]
 
-CRISPY_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
